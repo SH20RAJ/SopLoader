7830c4dd13ed39c1a4e3c685d3386d62a07e92b8
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2024-02-12 00:12:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var clipboard = new Clipboard('.clipboard');
 let code =(id,bg)=>{
   if(bg) var abg = "&bg="+bg; else abg = ""
   document.querySelector('#code').innerHTML = `
-  <pre class=" language-html"><code class=" language-html" id="code"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">src</span><span class="token attr-value"><span class="token punctuation">=</span><span class="token punctuation">"</span>https://sh20raj.github.io/SopLoader/sl.js?id=${id}&t=2${abg}<span class="token punctuation">"</span></span><span class="token punctuation">&gt;</span></span><span class="token script language-javascript"></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">&gt;</span></span>
+  <pre class=" language-html"><code class=" language-html" id="code"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span> <span class="token attr-name">src</span><span class="token attr-value"><span class="token punctuation">=</span><span class="token punctuation">"</span>https://cdn.jsdelivr.net/gh/sh20raj/soploader/sl.js?id=${id}&t=2${abg}<span class="token punctuation">"</span></span><span class="token punctuation">&gt;</span></span><span class="token script language-javascript"></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">&gt;</span></span>
 </code></pre>
 
   `;//  &#x3C;script src=&#x22;https://soploader.sh20raj.repl.co/sl.js?id=${id}&t=2${abg}&#x22;&#x3E;&#x3C;/script&#x3E;
